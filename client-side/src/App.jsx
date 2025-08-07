24861d1399a587844807f9a496e4ba94a2fae861
Kiru <kirushanthh13@gmail.com>
2025-08-07 18:46:15 +0530

    Initial commit

---
+++ client-side/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='m-1 text-3xl font-bold text-red-500 underline'>
+        Hello world?
+      </div>
+    </>
+  );
+}
+
+export default App;

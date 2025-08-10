27201fe658c039e4a0b853ebf28b17389f24f542
Abithan07 <asokumarabithan@gmail.com>
2025-08-10 12:08:28 +0530

    Home page

---
+++ client-side/src/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <div className='m-1 text-3xl font-bold text-red-500 underline'>
+        Hello world
+      </div>
+    </>
+  );
+}
+
+export default Home;

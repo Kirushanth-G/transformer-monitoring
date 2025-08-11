1f8a8ff6f467fc9a49a02026649291a9b9748855
Kiru <kirushanthh13@gmail.com>
2025-08-11 06:33:08 +0530

    modifications to frontend and backend creation

---
+++ server-transformer/src/main/java/com/kirus/server_transformer/ServerTransformerApplication.java
@@ -0,0 +1,13 @@
+package com.kirus.server_transformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerTransformerApplication.class, args);
+	}
+
+}
---
+++ server-transformer/src/test/java/com/kirus/server_transformer/ServerTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirus.server_transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
